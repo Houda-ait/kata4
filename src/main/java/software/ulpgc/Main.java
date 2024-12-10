4c3388b26062aeef0b505a5cd787577d73992a0e
houda <Houdaaitabdeslam@gmail.com>
2024-12-10 01:02:37 +0100

    V1, punto de partida : kata2

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -11,13 +11,7 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException, IOException {
-        File tsvFile = new File("title.basics.tsv");
-        File dbFile = new File("titles.db");
-        new TitleLoader().loadTitle(tsvFile, dbFile);
-        MainFrame mainFrame = new MainFrame();
-        SQLiteTitleReader titleReader = new SQLiteTitleReader(dbFile);
-        mainFrame.setTitleReader(titleReader);
-        mainFrame.setVisible(true);
+
     }
 
 }
